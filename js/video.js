12615e3fb43a92d5daa6304ce44c1ca03833b31b
shunaky <shunaky@umich.edu>
2023-03-30 15:00:07 -0400

    finished except styled and original

--- js/video.js
+++ js/video.js
@@ -1,11 +1,61 @@
-var video;
-
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	var video = document.querySelector(".video");
+	video.load();
+	console.log("Good job opening the window");
+	// TO DO : Initialize the video element and turn off autoplay and turn off looping
+	video.autoplay = false;
+	video.loop = false;
+	// document.querySelector("#volume").innerHTML = slider.value + "%";
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+window.onload=function(){
+	var video = document.querySelector(".video");
+	document.querySelector("#play").addEventListener("click", function() {
+		console.log("Play Video");
+		video.play();
+		// TO DO : update the volume info
+		video.volume = slider.value / 100;
+		document.querySelector("#volume").innerHTML = slider.value + "%";
+	});
 
+	document.querySelector("#pause").addEventListener("click", function() {
+		video.pause();
+	});
+
+	document.querySelector("#slower").addEventListener("click", function() {
+		var current_speed = video.playbackRate;
+		video.playbackRate = current_speed * 0.9;
+		console.log(video.playbackRate);
+	});
+
+	document.querySelector("#faster").addEventListener("click", function() {
+		var current_speed = video.playbackRate;
+		video.playbackRate = current_speed / 0.9;
+		console.log(video.playbackRate);
+	});
+
+	document.querySelector("#skip").addEventListener("click", function() {
+		var current_time = video.currentTime;
+		if (current_time + 10 > video.duration) {
+			video.currentTime = 0;
+		} else {
+			video.currentTime = current_time + 10;
+		}
+		console.log(video.currentTime);
+	});
+
+	document.querySelector("#mute").addEventListener("click", function() {
+		if (video.muted) {
+			document.querySelector("#mute").innerHTML = "Mute";
+		} else {
+			document.querySelector("#mute").innerHTML = "Unmute";
+		}
+		video.muted = !video.muted;
+	});
+
+	document.querySelector("#slider").addEventListener("change", function() {
+		var slider = document.querySelector("#slider");
+		video.volume = slider.value / 100;
+		document.querySelector("#volume").innerHTML = slider.value + "%";
+	});	
+};
